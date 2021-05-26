f5d4817e45e8d0bdc7b086d73f7dc017e67b544d
Akila De Silva <akiladesilva97@gmail.com>
2021-05-26 14:06:55 +0530

    front end

---
+++ front-end/medlink/src/App.js
@@ -0,0 +1,28 @@
+import { Container } from '@material-ui/core';
+import React from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import './App.css';
+import Home from './containers/Home'
+import Signin from './containers/Signin'
+import Signup from './containers/Signup'
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/Signin" component={Signin} />
+          <Route path="/Signup" component={Signup} />
+
+        </Switch>
+      </Router>
+      
+    </div>
+  );
+}
+
+export default App;
