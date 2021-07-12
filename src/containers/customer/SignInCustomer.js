a4848e9b6ddd7e42d5e4efb3226d6c817d23663a
MichelleFdo <mufernando02@gmail.com>
2021-07-12 23:22:26 +0530

    Update SignInCustomer.js

--- src/containers/customer/SignInCustomer.js
+++ src/containers/customer/SignInCustomer.js
@@ -6,6 +6,9 @@ import React, {useEffect, useState} from 'react';
 import { Grid } from '@material-ui/core';
 import Paper from '@material-ui/core/Paper';
 import Avatar from '@material-ui/core/Avatar';
+import Hidden from '@material-ui/core/Hidden';
+import LockOpenOutlinedIcon from '@material-ui/icons/LockOpenOutlined';
+
 //import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import LockOpenIcon from '@material-ui/icons/LockOpen';
 import { TextField } from '@material-ui/core';
@@ -15,11 +18,17 @@ import Button from '@material-ui/core/Button';
 import Link from '@material-ui/core/Link';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
+import { makeStyles } from '@material-ui/core/styles';
+
+import GridContainer from '../../components/Dashboard/Grid/GridContainer';
+import GridItem from '../../components/Dashboard/Grid/GridItem';
 
 import axios from 'axios';
 import {isuserLoggedIn, login} from '../../actions/auth.actionscustomer';
 import { useDispatch, useSelector } from 'react-redux';
 import { Redirect } from 'react-router-dom';
+
+import pharmacist from '../../assets/images/cust.jpg';
 
 
 function Copyright() {
@@ -34,8 +43,63 @@ function Copyright() {
       </Typography>
     );
   }
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      backgroundColor : '#eee',
+    },
+    paper: {
+      marginTop: theme.spacing(15),
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      padding:theme.spacing(4),
+      borderRadius: '25px',
+      boxShadow: `2px 2px 2px 2px #ccc`,
+    },
+    avatar: {
+      margin: theme.spacing(1),
+      backgroundColor: theme.palette.primary.main,
+    },
+    form: {
+      width: '100%', // Fix IE 11 issue.
+      marginTop: theme.spacing(2),
+    },
+    submit: {
+      backgroundColor: theme.palette.primary.main,
+
+      margin: theme.spacing(3, 0, 1),
+      width:'70%',
+    },
+    /*label:{
+      marginLeft:theme.spacing(0),
+    // },*/
+    // back:{
+    //   backgroundColor: "#eee",
+    //   marginTop:theme.spacing(1),
+    //   margin:theme.spacing(1,1,1),
+    //   height:'small',
+    // },
+    input: {
+      display: 'none',
+    },
+    image: {
+      alignItems: 'center',
+      justifyContent: 'center',
+      width:"75%",
+      // height:'auto',
+      // maxHeight:'400px',
+      
+      padding:theme.spacing(4),
+      marginTop:theme.spacing(5),
+      maxWidth:'600px',
+    },
+    
+  
+  }));
 
 const SignInCustomer=()=>{
+  const classes = useStyles();
+
     const [email,setEmail] = useState ('');
     const [password,setPassword] = useState ('');
     const [error,setError] = useState ('');
@@ -66,49 +130,87 @@ const SignInCustomer=()=>{
           return <Redirect to={'/customer'} />
       }
 
-    const paperStyle={padding :20,height:'450px',width:'300px', margin:"20px auto"}
-    const avatarStyle={backgroundColor: '#2ab5b5'}
-    const gridStyle={padding: 20}
-    const buttonStyle={backgroundColor: '#2ab5b5', margin: '8px 0'}
-     
-    return(
-        
-        <Grid style={gridStyle}>
-            <Paper elevation={10} style={paperStyle}>
-                <Grid align='center' style={gridStyle}>
-                    <Avatar style={avatarStyle}><LockOpenIcon/></Avatar>
-                    <h1>Sign in</h1>
-                </Grid>
-                    <TextField value ={email} onChange={(e) => setEmail(e.target.value)} id="lastName" id="email" label="Email" placeholder="Enter Your Email" fullWidth required/>
-                    <TextField value ={password} onChange={(e) => setPassword(e.target.value)} id="lastName" id="password" label="Password" placeholder="Enter Password" type='password' fullWidth required/>
-                    <FormControlLabel  
-                        control={
-                            <Checkbox 
-                            name="checkedB"
-                            color="Primary"
-                            />
-                        }
-                        label="Remember me"
-                    />
-                
-                <Button type='submit' onClick= {userLogin} variant="contained" style={buttonStyle} href="" fullWidth>Sign in</Button>
-                <Typography>
-                    <Link href="#">
-                        Forgot Password ?
-                    </Link>
-                </Typography>
+      
+    // const paperStyle={padding :20,height:'450px',width:'300px', margin:"20px auto"}
+    // const avatarStyle={backgroundColor: '#2ab5b5'}
+    // // const gridStyle={padding: 20}
+    // const buttonStyle={backgroundColor: '#2ab5b5', margin: '8px 0'}
 
-                <Typography>Do you have an account ? 
-                    <Link href="/customersignup">
-                        Sign Up
-                    </Link>
-                </Typography>
-                
-            </Paper>
-            <Box mt={5}>
-                <Copyright />
-            </Box>
-        </Grid>
+    return(
+        <div>
+        <GridContainer spacing={0}>
+          <Hidden smDown>
+              <GridItem xs={12} sm={12} md={7}>
+                  <img src={pharmacist} className={classes.image} />
+              </GridItem>
+          </Hidden>
+        <GridItem xs={12} sm={12} md={4} spacing={3}  borderRadius={10}>
+
+          <div className={classes.paper}>
+            <Avatar className={classes.avatar}>
+              <LockOpenOutlinedIcon />
+            </Avatar>
+          
+            <Typography component="h1" variant="h5">
+              Customer SignIn
+            </Typography>
+            <form className={classes.form} noValidate>
+              <Grid container spacing={2}>
+                          <TextField value ={email} onChange={(e) => setEmail(e.target.value)} id="lastName" id="email" label="Email" placeholder="Enter Your Email" fullWidth required/>
+                          <TextField value ={password} onChange={(e) => setPassword(e.target.value)} id="lastName" id="password" label="Password" placeholder="Enter Password" type='password' fullWidth required/>
+              </Grid> 
+              <Grid item xs={12}>         
+                          <FormControlLabel  
+                              control={
+                                  <Checkbox 
+                                  name="checkedB"
+                                  color="Primary"
+                                  />
+                              }
+                              label="Remember me"
+                          />
+              </Grid>   
+              <Box xs={12} sm={12} md={12}
+                alignItems="center" 
+                display="flex" 
+                justifyContent="center"
+                direction="column"
+                justify="center">    
+                      <Button 
+                        type='submit' 
+                        onClick= {userLogin} 
+                        variant="contained" href="" 
+                        fullWidth
+                        className={classes.submit}>
+                          Sign in</Button>
+                </Box>
+                <Grid container justify="flex-end">
+                  <Grid item>
+                    <Typography>
+                            <Link href="#">
+                                Forgot Password ?
+                            </Link>
+                    </Typography>
+                  </Grid>
+                  </Grid>
+                  <Grid container justify="flex-end">
+                    <Grid item>
+                      <Typography>Do you have an account ? 
+                            <Link href="/customersignup">
+                                Sign Up
+                            </Link>
+                      </Typography>
+                    </Grid>
+                  </Grid>     
+              </form>
+          </div> 
+        </GridItem>
+      
+        </GridContainer>
+        <Box xs={12} sm={12} md={12} >
+          <Copyright />
+        </Box>
+      </div>
     )
 }
 
