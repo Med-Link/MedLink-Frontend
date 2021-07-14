eb2468b8d1f8ad997099aa1d8a50f9d83436e117
Akila De Silva <akiladesilva97@gmail.com>
2021-07-14 11:23:23 +0530

    presentation ready

--- src/App.js
+++ src/App.js
@@ -48,17 +48,17 @@ function App() {
           <Route path="/ResetPassword" component={ResetPassword} />
            
 
-          <Route path="/admin" component={Admin} />
+          <PrivateRoute path="/admin" component={Admin} />
           {/* <Route path="/adminsignup" component={SignUpAdmin} /> */}
 
           <Route path="/adminsignin" component={SignInAdmin} />
 
-          <Route path="/Customer" component={Customer} />
+          <PrivateRoute1 path="/Customer" component={Customer} />
           <Route path="/customersignup" component={CustomerSignup} />
           <Route path="/customersignin" component={CustomerSignin} />
 
           {/* <PrivateRoute2 path="/pharmacy" component={Pharmacy} /> */}
-          <Route path="/pharmacy" component={Pharmacy} />
+          <PrivateRoute2 path="/pharmacy" component={Pharmacy} />
           <Route path="/pharmacysignup" component={SignUpPharmacy} />
           <Route path="/pharmacysignin" component={SignInPharmacy} />
 
