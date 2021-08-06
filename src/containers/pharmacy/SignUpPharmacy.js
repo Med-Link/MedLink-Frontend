5db6891494a48cef41b2f69aafc5b5b23d8198af
MichelleFdo <mufernando02@gmail.com>
2021-08-07 02:14:50 +0530

    signup pharmacy footer and margins updated

--- src/containers/pharmacy/SignUpPharmacy.js
+++ src/containers/pharmacy/SignUpPharmacy.js
@@ -49,7 +49,7 @@ const useStyles = makeStyles((theme) => ({
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
-    padding:theme.spacing(4),
+    padding:theme.spacing(3),
     borderRadius: '25px',
     boxShadow: `2px 2px 2px 2px #ccc`,
   },
@@ -62,30 +62,30 @@ const useStyles = makeStyles((theme) => ({
     marginTop: theme.spacing(2),
   },
   submit: {
-    margin: theme.spacing(3, 0, 1),
+    margin: theme.spacing(2, 0, 1),
     width:'70%',
   },
-  /*label:{
-    marginLeft:theme.spacing(0),
-  // },*/
-  // back:{
-  //   backgroundColor: "#eee",
-  //   marginTop:theme.spacing(1),
-  //   margin:theme.spacing(1,1,1),
-  //   height:'small',
-  // },
   input: {
-    display: 'none',
+    // display: 'none',
   },
+  // image: {
+  //   alignItems: 'center',
+  //   justifyContent: 'flex-end',
+  //   height:'100%',
+  //   // height:'auto',
+  //   // maxHeight:'400px',
+    
+  //   padding:theme.spacing(4),
+  //   maxWidth:'900px',
+  // },
   image: {
     alignItems: 'center',
-    justifyContent: 'flex-end',
-    height:'100%',
-    // height:'auto',
-    // maxHeight:'400px',
-    
-    padding:theme.spacing(4),
-    maxWidth:'900px',
+    justifyContent: 'center',
+    width:"75%",
+    marginLeft:theme.spacing(8),
+    padding:theme.spacing(2),
+    marginTop:theme.spacing(3),
+    maxWidth:'600px',
   },
   
 
@@ -160,217 +160,211 @@ const SignUpPharmacy=()=>{
   
   return (
     <div style={{overflow: "hidden"}}>
-    <GridContainer spacing={0} >
-    <Hidden smDown>
-      <GridItem xs={12} sm={12} md={7}>
-        <img src={pharmacist} className={classes.image} />
-      </GridItem>
-      </Hidden>
-      <GridItem xs={12} sm={12} md={4}  borderRadius={10}>
-
-      <div className={classes.paper}>
-        
-        <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
-        </Avatar>
-        
-        <Typography component="h1" variant="h5">
-          Pharmacy Sign up
-        </Typography>
-        
-        <form className={classes.form} noValidate>
+      <GridContainer spacing={0} >
+        <Hidden smDown>
+          <GridItem xs={12} sm={12} md={7}>
+            <img src={pharmacist} className={classes.image} />
+          </GridItem>
+        </Hidden>
+        <GridItem xs={12} sm={12} md={4}  borderRadius={10}>
+          <div className={classes.paper}>
+            <Avatar className={classes.avatar}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Pharmacy Sign up
+            </Typography>
           
-          <Grid container spacing={2}>
-            
-            <Grid item xs={12} sm={6}>
-              <TextField
-                autoComplete="pname"
-                value ={name} onChange={(e) => setName(e.target.value)}
-                name="pharmacyName"
-                variant="outlined"
-                required
+            <form className={classes.form} noValidate>
+              
+              <Grid container spacing={2}>
+                
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    autoComplete="pname"
+                    value ={name} onChange={(e) => setName(e.target.value)}
+                    name="pharmacyName"
+                    variant="outlined"
+                    required
+                    fullWidth
+                    id="pharmacyName"
+                    label="Pharmacy Name"
+                    autoFocus
+                    size="small"
+                  />
+                </Grid>
+                
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    variant="outlined"
+                    value ={contactNumber} onChange={(e) => setContactNumber(e.target.value)}
+                    required
+                    fullWidth
+                    id="pNumber"
+                    label="Mobile Number"
+                    name="pNumber"
+                    size="small"
+                  />
+                </Grid>
+                
+                <Grid item xs={12}>
+                  <TextField
+                    variant="outlined"
+                    value ={email} onChange={(e) => setEmail(e.target.value)}
+                    required
+                    fullWidth
+                    id="email"
+                    label="Email Address"
+                    name="email"
+                    size="small"
+                  />
+                </Grid>
+                
+                <Grid item xs={12}>
+                  <TextField
+                    variant="outlined"
+                    value ={password} onChange={(e) => setPassword(e.target.value)}
+                    required
+                    fullWidth
+                    name="password"
+                    label="Password"
+                    type="password"
+                    id="password"
+                    size="small"
+                  />
+                </Grid>
+                
+                <Grid item xs={12}>
+                  <TextField
+                    variant="outlined"
+                    value ={location} onChange={(e) => setLocation(e.target.value)}
+                    required
+                    fullWidth
+                    id="location"
+                    label="Location URL"
+                    name="location"
+                    size="small"
+                  />
+                </Grid>
+                
+                <Grid item xs={12} sm={12} md={6} >
+                  <Typography variant="body2" display="block">
+                    Medical Council Certificate *
+                  </Typography>                
+                </Grid>
+                <Grid item xs={12}  sm={12} md={6}>
+                <input
+                  accept="image/*"
+                  onChange={handleRegisterDocs}
+                  className={classes.input}
+                  id="contained-button-file"
+                  multiple
+                  type="file"
+                />
+                {/* <label htmlFor="contained-button-file">
+                  <Button 
+                    variant="contained" 
+                    component="span" 
+                    color="default"
+                    startIcon={<CloudUploadIcon />}>
+                  Upload
+                  </Button>
+                </label>              */}
+                </Grid>
+                
+                <Grid item xs={12} sm={12} md={6}>
+                  <Typography variant="body2" display="block" >
+                    Pharmaciest Licence *
+                  </Typography>                
+                </Grid>
+                <Grid item xs={12} sm={12} md={6}>
+                  <input
+                    accept="image/*"
+                  onChange={handleRegisterDocs}
+                    className={classes.input}
+                    id="contained-button-file"
+                    multiple
+                    type="file"
+                  />
+                  {/* <label htmlFor="contained-button-file">
+                    <Button 
+                      variant="contained" 
+                      component="span" 
+                      color="default"
+                      startIcon={<CloudUploadIcon />}>
+                    Upload
+                    </Button>
+                  </label>                  */}
+                </Grid>
+
+                <Grid item xs={12} sm={12} md={6}>
+                  <Typography variant="body2" display="block">
+                    Business Registration Certificate *
+                  </Typography>                
+                </Grid>
+                <Grid item xs={12} sm={12} md={6}>
+                  <input
+                    accept="image/*"
+                    onChange={handleRegisterDocs}
+                    className={classes.input}
+                    id="contained-button-file"
+                    multiple
+                    type="file"
+                  />
+                  {/* <label htmlFor="contained-button-file">
+                    <Button 
+                      variant="contained" 
+                      component="span" 
+                      color="default"
+                      startIcon={<CloudUploadIcon />}>
+                    Upload
+                    </Button>
+                  </label>                 */}
+                </Grid>
+                
+                <Grid item xs={12}>
+                  <FormControlLabel
+                    onChange={(e) => setChecked(e.target.checked)}
+                    control={<Checkbox value="allowExtraEmails" color="primary" />}
+                    label="I agree to the Terms and Conditions"
+                  />
+                </Grid>
+
+              </Grid>
+              <Box xs={12} sm={12} md={12}
+              alignItems="center" 
+              display="flex" 
+              justifyContent="center"
+              direction="column"
+              justify="center">
+                <Button
+                type="button"
                 fullWidth
-                id="pharmacyName"
-                label="Pharmacy Name"
-                autoFocus
-                size="small"
-              />
-            </Grid>
-            
-            <Grid item xs={12} sm={6}>
-              <TextField
-                variant="outlined"
-                value ={contactNumber} onChange={(e) => setContactNumber(e.target.value)}
-                required
-                fullWidth
-                id="pNumber"
-                label="Mobile Number"
-                name="pNumber"
-                size="small"
-              />
-            </Grid>
-            
-            <Grid item xs={12}>
-              <TextField
-                variant="outlined"
-                value ={email} onChange={(e) => setEmail(e.target.value)}
-                required
-                fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                size="small"
-              />
-            </Grid>
-            
-            <Grid item xs={12}>
-              <TextField
-                variant="outlined"
-                value ={password} onChange={(e) => setPassword(e.target.value)}
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                size="small"
-              />
-            </Grid>
-            
-            <Grid item xs={12}>
-              <TextField
-                variant="outlined"
-                value ={location} onChange={(e) => setLocation(e.target.value)}
-                required
-                fullWidth
-                id="location"
-                label="Location URL"
-                name="location"
-                size="small"
-              />
-            </Grid>
-            
-            <Grid item xs={12} sm={12} md={8} >
-              <Typography variant="body2" display="block">
-                Medical Council Certificate *
-              </Typography>                
-            </Grid>
-            <Grid item xs={12}  sm={12} md={4}>
-            <input
-              accept="image/*"
-              onChange={handleRegisterDocs}
-              className={classes.input}
-              id="contained-button-file"
-              multiple
-              type="file"
-            />
-            <label htmlFor="contained-button-file">
-              <Button 
-                variant="contained" 
-                component="span" 
-                color="default"
-                startIcon={<CloudUploadIcon />}>
-              Upload
+                variant="contained"
+                color="primary"
+                onClick={Signup}
+                className={classes.submit}
+
+              >
+                Sign Up
               </Button>
-            </label>             
-            </Grid>
-            
-            <Grid item xs={12} sm={12} md={8}>
-              <Typography variant="body2" display="block" >
-                Pharmaciest Licence *
-              </Typography>                
-            </Grid>
-            <Grid item xs={12} sm={12} md={4}>
-              <input
-                accept="image/*"
-              onChange={handleRegisterDocs}
-                className={classes.input}
-                id="contained-button-file"
-                multiple
-                type="file"
-              />
-              <label htmlFor="contained-button-file">
-                <Button 
-                  variant="contained" 
-                  component="span" 
-                  color="default"
-                  startIcon={<CloudUploadIcon />}>
-                Upload
-                </Button>
-              </label>                 
-            </Grid>
-
-            <Grid item xs={12} sm={12} md={8}>
-              <Typography variant="body2" display="block">
-                Business Registration Certificate *
-              </Typography>                
-            </Grid>
-            <Grid item xs={12} sm={12} md={4}>
-              <input
-                accept="image/*"
-                onChange={handleRegisterDocs}
-                className={classes.input}
-                id="contained-button-file"
-                multiple
-                type="file"
-              />
-              <label htmlFor="contained-button-file">
-                <Button 
-                  variant="contained" 
-                  component="span" 
-                  color="default"
-                  startIcon={<CloudUploadIcon />}>
-                Upload
-                </Button>
-              </label>                
-            </Grid>
-            
-            <Grid item xs={12}>
-              <FormControlLabel
-                onChange={(e) => setChecked(e.target.checked)}
-                control={<Checkbox value="allowExtraEmails" color="primary" />}
-                label="I agree to the Terms and Conditions"
-              />
-            </Grid>
-
-          </Grid>
-          <Box xs={12} sm={12} md={12}
-          alignItems="center" 
-          display="flex" 
-          justifyContent="center"
-          direction="column"
-          justify="center">
-            <Button
-            type="button"
-            fullWidth
-            variant="contained"
-            color="primary"
-            onClick={Signup}
-            className={classes.submit}
-
-          >
-            Sign Up
-          </Button>
-          </Box>
-          <Grid container justify="flex-end">
-            <Grid item>
-              <Link href="/pharmacysignin" variant="body2">
-                Already have an account? Sign in
-              </Link>
-            </Grid>
-          </Grid>
-          
-        </form>
-      </div>
-      
-      
-      </GridItem>
-      
+              </Box>
+              <Grid container justify="flex-end">
+                <Grid item>
+                  <Link href="/pharmacysignin" variant="body2">
+                    Already have an account? Sign in
+                  </Link>
+                </Grid>
+              </Grid>
+              
+            </form>
+          </div>
+        </GridItem>
       </GridContainer>
       <Box xs={12} sm={12} md={12} >
         <Footer />
       </Box>
-      </div>
+    </div>
   );
 }
 export default SignUpPharmacy ;
