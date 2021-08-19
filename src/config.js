1b5a0a217f76a4ed7492aeb45a5dc7e09626c6e5
MichelleFdo <mufernando02@gmail.com>
2021-08-19 21:27:31 +0530

    Revert "map config file"
    This reverts commit 6af1725cc866d59b6df3729a5aab3b0f877d333e.

--- src/config.js
+++
@@ -1,8 +0,0 @@
-// export const GoogleMapsAPI = 'AIzaSyCIhmOKfurA9XKCt_4N8tvZj7so86bTtU4';
-const dotenv = require('dotenv');
-dotenv.config();
-
-const config = {
-    GoogleMapsAPI: process.env.REACT_APP_GOOGLEMAPS_API_KEY
-}
-module.exports = config;
