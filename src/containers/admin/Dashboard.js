8462502e8170d92e829893b2043045dbf05c8e1e
SandaliPerera <hazsanu@gmail.com>
2021-09-07 17:20:08 +0530

    7/9

--- src/containers/admin/Dashboard.js
+++ src/containers/admin/Dashboard.js
@@ -43,6 +43,7 @@ export default function Dashboard() {
         }
         })
       .then(res =>{
+
         const results =  res.data.row;
         //  console.log(results);
         setData(results);
