62974fd90d89ed014cc7d957de20264a413bea1e
MichelleFdo <mufernando02@gmail.com>
2021-09-10 03:05:05 +0530

    Update AddNewMed.js

--- src/containers/pharmacy/forms/AddNewMed.js
+++ src/containers/pharmacy/forms/AddNewMed.js
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, {useState} from "react";
+import axios from 'axios';
+import { backendUrl } from "../../../urlConfig.js";
+
 import { TextField } from '@material-ui/core';
 import { makeStyles } from "@material-ui/core/styles";
+import Autocomplete from '@material-ui/lab/Autocomplete';
 
 import GridItem from "../../../components/Dashboard/Grid/GridItem.js";
 import GridContainer from "../../../components/Dashboard/Grid/GridContainer.js";
@@ -12,9 +16,38 @@ import styles from "../../../assets/jss/material-dashboard-react/views/dashboard
 
 const useStyles = makeStyles(styles);
 
+
 const AddNewMed=()=>{  
     const classes = useStyles();
-   
+
+    //backend connection for medicine drop down list
+    const [meddata, setMedData] = useState([]);
+
+    const getMedData =() =>{
+        const token = window.localStorage.getItem('token');
+        axios.get(`${backendUrl}/pharmacy/listmedicine`,{
+            headers: {
+            'Authorization': token ? `Bearer ${token}` : ''
+            }
+            })
+        .then(res =>{
+            const results =  res.data.result;
+            setMedData(results);
+        })
+    }
+
+    React.useEffect(()=>{
+        getMedData();
+    },[]);
+
+    const options = meddata.map((option) => {
+        const firstLetter = option.medname[0].toUpperCase();
+        return {
+            firstLetter: /[0-9]/.test(firstLetter) ? '0-9' : firstLetter,
+            ...option,
+        };
+    });
+
     return(
     <div>
         <Card >
@@ -25,51 +58,76 @@ const AddNewMed=()=>{
 
             <GridContainer>
                 <GridItem xs={6} sm={6} md={6}>
-                    <TextField
-                        id="firstName"
-                        label="Medicine Name"
-                        InputProps={{
-                            readOnly: false,
-                        }}
-                        fullWidth
+                    <Autocomplete
+                        disableClearable
+                        options={options.sort((a, b) => -b.firstLetter.localeCompare(a.firstLetter))}
+                        groupBy={(option) => option.firstLetter}
+                        getOptionLabel={(option) => option.medname+" - "+option.brand}
+                        renderInput={(params) => <TextField {...params} label="Medicine Name" required />}
+                        size="small"
                     />
                 </GridItem>
-                <GridItem xs={6} sm={6} md={6}>
+                <GridItem xs={3} sm={3} md={3}>
                     <TextField
-                        id="lastName"
-                        label="Brand Name"
+                        id="quantity"
+                        label="Quantity"
                         InputProps={{
                             readOnly: false,
                         }}
                         fullWidth
+                        required
+                    />
+                </GridItem>
+                <GridItem xs={3} sm={3} md={3}>
+                    <TextField
+                        id="price"
+                        label="UnitPrice"
+                        InputProps={{
+                            readOnly: false,
+                        }}
+                        fullWidth
+                        required
                     />
                 </GridItem>
             </GridContainer>
             <GridContainer>
+                
                 <GridItem xs={6} sm={6} md={6}>
                     <TextField
-                        id="email"
-                        label="Re-Order Level"
-                        InputProps={{
-                            readOnly: false,
-                        }}
+                        autoFocus
+                        type="date"
+                        margin="dense"
+                        variant="standard"
+                        id="mnfdate"
+                        label="Manufactured Date"
                         fullWidth
+                        InputLabelProps={{
+                        shrink: true,
+                        }}
+                        size="small"
+                        required
                     />
-                </GridItem>
+            </GridItem>
                 <GridItem xs={6} sm={6} md={6}>
                     <TextField
-                        id="address"
-                        label="Medicine ID"
-                        InputProps={{
-                            readOnly: true,
-                        }}
+                        autoFocus
+                        type="date"
+                        margin="dense"
+                        variant="standard"
+                        id="expdate"
+                        label="Expire Date"
                         fullWidth
+                        InputLabelProps={{
+                        shrink: true,
+                        }}
+                        size="small"
+                        required
                     />
                 </GridItem>                    
             </GridContainer>   
-            <GridContainer style={{display: "flex",justifyContent: "center", alignItems: "center",}}>
-                <Button type='submit' href="#" color="success" style={{marginTop:"10px"}}>Add</Button>
-            </GridContainer>
+                <GridContainer style={{display: "flex",justifyContent: "center", alignItems: "center",}}>
+                    <Button type='submit' href="#" color="success" style={{marginTop:"20px"}}>Add</Button>
+                </GridContainer>
             </CardBody>
         </Card>
     </div>
