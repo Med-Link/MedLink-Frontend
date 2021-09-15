6a876bc7f92a468bbfbe1cb137b9ffa9b828cdf7
Piyumi Senarath <piyumisenarath12@gmail.com>
2021-09-16 01:22:40 +0530

    Create some modifications in reset password component

---
+++ src/containers/customer/password/ResetPasswordPage.js
@@ -0,0 +1,213 @@
+import React from 'react';
+import { Grid } from '@material-ui/core';
+import Paper from '@material-ui/core/Paper';
+import Avatar from '@material-ui/core/Avatar';
+//import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import LockOpenIcon from '@material-ui/icons/LockOpen';
+import { TextField } from '@material-ui/core';
+import Checkbox from '@material-ui/core/Checkbox';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Button from '@material-ui/core/Button';
+import Link from '@material-ui/core/Link';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import AccountBoxIcon from '@material-ui/icons/AccountBox';
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
+import IconButton from '@material-ui/core/IconButton';
+import Input from '@material-ui/core/Input';
+import FilledInput from '@material-ui/core/FilledInput';
+import OutlinedInput from '@material-ui/core/OutlinedInput';
+import InputLabel from '@material-ui/core/InputLabel';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControl from '@material-ui/core/FormControl';
+import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
+
+function Copyright() {
+    return (
+      <Typography variant="body2" color="textSecondary" align="center">
+        {'Copyright © '}
+        <Link color="inherit" href="/">
+          Medlink
+        </Link>{' '}
+        {new Date().getFullYear()}
+        {'.'}
+      </Typography>
+    );
+  }
+
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      display: 'flex',
+      flexWrap: 'wrap',
+    },
+    margin: {
+      margin: theme.spacing(1),
+    },
+    withoutLabel: {
+      marginTop: theme.spacing(3),
+    },
+    textField: {
+      width: '95%',
+    },
+  }));
+
+const ResetPassword=()=>{
+
+    const classes = useStyles();
+  const [values, setValues] = React.useState({
+    amount: '',
+    password: '',
+    weight: '',
+    weightRange: '',
+    showPassword: false,
+  });
+
+  const handleChange = (prop) => (event) => {
+    setValues({ ...values, [prop]: event.target.value });
+  };
+
+  const handleClickShowPassword = () => {
+    setValues({ ...values, showPassword: !values.showPassword });
+  };
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
+    const paperStyle={padding :20,height:'500px',width:'400px', margin:"20px auto"}
+    const avatarStyle={backgroundColor: '#126e82'}
+    const gridStyle={padding: 20}
+    const buttonStyle={color: '#efe3e3',backgroundColor: '#126e82', margin: '30px 0', width: '100%', left:'50%'}
+    const headingStyle = {color: '#126e82'};
+    const inputStyle = {width: '700px'}
+
+    return(
+        
+        <Grid style={gridStyle}>
+             <Paper style={paperStyle}> 
+                <Grid align='center' style={gridStyle}>
+                     
+                    <h1 style={headingStyle}>Reset Password</h1>
+                </Grid>
+
+                <Grid container spacing={2}> 
+                    <Grid item xs={12}>
+                        {/*<TextField
+                            id="currentPassword"
+                            label="Current Password"
+                            type='password'
+                            variant="outlined"
+                            fullWidth
+                        />*/}
+                        <FormControl className={clsx(classes.margin, classes.textField)} variant="outlined">
+                            <InputLabel style={inputStyle} htmlFor="outlined-adornment-password">Current Password</InputLabel>
+                            <OutlinedInput
+                            id="outlined-adornment-password"
+                            type={values.showPassword ? 'text' : 'password'}
+                            value={values.password}
+                            onChange={handleChange('password')}
+                            
+                            endAdornment={
+                                <InputAdornment position="end">
+                                <IconButton
+                                    aria-label="toggle password visibility"
+                                    onClick={handleClickShowPassword}
+                                    onMouseDown={handleMouseDownPassword}
+                                    edge="end"
+                                >
+                                {values.showPassword ? <Visibility /> : <VisibilityOff />}
+                                </IconButton>
+                                </InputAdornment>
+                            }
+                            labelWidth={200}
+                        />
+                        </FormControl>
+                    </Grid>
+
+                    <Grid item xs={12}>
+                        {/*<TextField
+                            id="currentPassword"
+                            label="Current Password"
+                            type='password'
+                            variant="outlined"
+                            fullWidth
+                        />*/}
+                        <FormControl className={clsx(classes.margin, classes.textField)} variant="outlined">
+                            <InputLabel style={inputStyle} htmlFor="outlined-adornment-password">New Password</InputLabel>
+                            <OutlinedInput
+                            id="outlined-adornment-password"
+                            type={values.showPassword ? 'text' : 'password'}
+                            value={values.password}
+                            onChange={handleChange('password')}
+                            
+                            endAdornment={
+                                <InputAdornment position="end">
+                                <IconButton
+                                    aria-label="toggle password visibility"
+                                    onClick={handleClickShowPassword}
+                                    onMouseDown={handleMouseDownPassword}
+                                    edge="end"
+                                >
+                                {values.showPassword ? <Visibility /> : <VisibilityOff />}
+                                </IconButton>
+                                </InputAdornment>
+                            }
+                            labelWidth={200}
+                        />
+                        </FormControl>
+                    </Grid>
+
+                    <Grid item xs={12}>
+                        {/*<TextField
+                            id="currentPassword"
+                            label="Current Password"
+                            type='password'
+                            variant="outlined"
+                            fullWidth
+                        />*/}
+                        <FormControl className={clsx(classes.margin, classes.textField)} variant="outlined">
+                            <InputLabel style={inputStyle} htmlFor="outlined-adornment-password">Confirm New Password</InputLabel>
+                            <OutlinedInput
+                            id="outlined-adornment-password"
+                            type={values.showPassword ? 'text' : 'password'}
+                            value={values.password}
+                            onChange={handleChange('password')}
+                            
+                            endAdornment={
+                                <InputAdornment position="end">
+                                <IconButton
+                                    aria-label="toggle password visibility"
+                                    onClick={handleClickShowPassword}
+                                    onMouseDown={handleMouseDownPassword}
+                                    edge="end"
+                                >
+                                {values.showPassword ? <Visibility /> : <VisibilityOff />}
+                                </IconButton>
+                                </InputAdornment>
+                            }
+                            labelWidth={200}
+                        />
+                        </FormControl>
+                    </Grid>
+
+                     
+                     
+
+                    <Grid item xs={6}>
+                        <Button type='submit' variant="contained" style={buttonStyle} href="">Submit</Button>
+                    </Grid>
+
+                     
+                </Grid> 
+
+                 
+                
+        </Paper>  
+        </Grid>
+    )
+}
+
+export default ResetPassword
