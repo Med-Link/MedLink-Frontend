a307c216b11644090ae48b3586f0d12d8dcad2cc
Akila De Silva <akiladesilva97@gmail.com>
2021-10-05 14:43:45 +0530

    connecting back to localhost backend

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,5 +1,5 @@
-// export const backendUrl = 'http://localhost:4000/api';
-export const backendUrl = 'https://medlinkserver.herokuapp.com/api';
+export const backendUrl = 'http://localhost:4000/api';
+// export const backendUrl = 'https://medlinkserver.herokuapp.com/api';
 
 export const ngrokUrl = 'https://62cf-2402-4000-13c6-f0ec-a567-3bf4-ad30-2749.ngrok.io/api';
 
